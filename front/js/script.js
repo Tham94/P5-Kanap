c8305573bf902e1df08ce047f41459828ee7748e
Tham94 <tham_the_indian@msn.com>
2022-05-24 10:18:50 +0200

    making code cleaner: add "-" in class card-content

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,33 @@
 
 function addLink(data, index){ //Ajouter une balise <a>
     let a = document.createElement("a");
-    a.setAttribute("class", "card-link"+index);
+    a.setAttribute("class", "card-link-"+index);
     a.setAttribute("href", `./product.html?id=${data[index]._id}`);
     document.getElementById("items").appendChild(a);
 }
 function addArticle(index){ //Ajouter une balise <article>
     let article = document.createElement("article");
-    article.setAttribute("class", "card-content"+index);
-    document.getElementsByClassName("card-link"+index)[0].appendChild(article);
+    article.setAttribute("class", "card-content-"+index);
+    document.getElementsByClassName("card-link-"+index)[0].appendChild(article);
 }
 function addProductImage(data, index){ //Ajouter une balise <img>
     let image = document.createElement("img");
     image.setAttribute("class","card-content__image");
     image.setAttribute("src",data[index].imageUrl);
     image.setAttribute("alt",data[index].altTxt);
-    document.getElementsByClassName("card-content"+index)[0].appendChild(image); 
+    document.getElementsByClassName("card-content-"+index)[0].appendChild(image); 
 }
 function addProductTitle(data, index){ //Ajouter une balise <h3>
     let productTitle = document.createElement("h3");
     productTitle.setAttribute("class","productName");
     productTitle.textContent= data[index].name;
-    document.getElementsByClassName("card-content"+index)[0].appendChild(productTitle);
+    document.getElementsByClassName("card-content-"+index)[0].appendChild(productTitle);
 }
 function addProductDescription(data, index){ //Ajouter une balise <p>
     let productDescription = document.createElement("p"); 
     productDescription.setAttribute("class","productDescription");
     productDescription.textContent= data[index].description;
-    document.getElementsByClassName("card-content"+index)[0].appendChild(productDescription);
+    document.getElementsByClassName("card-content-"+index)[0].appendChild(productDescription);
 }
 
 async function init() {
