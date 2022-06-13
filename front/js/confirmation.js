e0a9164f0c46928d60ce2667495a8894b99da208
Tham94 <tham_the_indian@msn.com>
2022-06-13 14:25:59 +0200

    confirmation.js : clear localStorage only

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,19 +1,15 @@
 // récupération URL actuelle
-let currentUrl = window.location.href; 
+let currentUrl = window.location.href;
 let url = new URL(currentUrl);
 
 // récupération de l'orderId à partir de l'url actuelle pour pouvoir l'afficher dans la page
-let orderId = url.searchParams.get("orderId"); 
+let orderId = url.searchParams.get("orderId");
 
 // affichage du numéro de commande
 const txtOrderId = document.getElementById('orderId');
-txtOrderId.textContent = orderId ;
+txtOrderId.textContent = orderId;
 
-
-// effacement de l'orderId de stockage et RAZ du panier
-setTimeout (() => {
-    sessionStorage.clear();
-    localStorage.clear();
-},50);
+// RAZ du panier
+localStorage.clear();
 
 
